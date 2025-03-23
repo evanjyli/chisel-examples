35d6b042f82b349d08dd4a53d067d66daf081419
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-03-23 12:08:37 -0700

    Add aggregate sram example

--- src/main/scala/gcd/SRAM.scala
+++ src/main/scala/gcd/SRAM.scala
@@ -45,6 +45,29 @@ class SinglePortSRAM(width: Int) extends Module {
   io.rdata := mem.read(io.raddr, !io.wen)
 }
 
+class AggregateBundle extends Bundle {
+  val a = UInt(2.W)
+  val b = SInt(3.W)
+}
+
+class AggregateSRAM(width: Int) extends Module {
+  val io = IO(new Bundle {
+    val raddr = Input(UInt(3.W))
+    val rdata = Output(Vec(4, new AggregateBundle))
+
+    val wen = Input(Bool())
+    val waddr = Input(UInt(3.W))
+    val wdata = Input(Vec(4, new AggregateBundle))
+    val wmask = Input(Vec(4, Bool()))
+  })
+
+  val mem = SyncReadMem(8, Vec(4, new AggregateBundle))
+  when (io.wen) {
+    mem.write(io.waddr, io.wdata, io.wmask)
+  }
+
+  io.rdata := mem.read(io.raddr, !io.wen)
+}
 
 object SRAM extends App {
   ChiselStage.emitSystemVerilogFile(
@@ -74,4 +97,18 @@ object SRAM extends App {
   val fileWriter2 = new PrintWriter("OneReadOneWritePortSRAM.fir")
   fileWriter2.write(chirrtl2)
   fileWriter2.close()
+
+  ChiselStage.emitSystemVerilogFile(
+    new AggregateSRAM(2),
+    firtoolOpts = Array(
+      "-disable-all-randomization",
+      "-strip-debug-info",
+      "--lowering-options=disallowLocalVariables,noAlwaysComb,verifLabels,disallowPortDeclSharing"))
+
+  val chirrtl3 = ChiselStage.emitCHIRRTL(
+    new AggregateSRAM(2)
+  )
+  val fileWriter3 = new PrintWriter("AggregateSRAM.fir")
+  fileWriter3.write(chirrtl3)
+  fileWriter3.close()
 }
