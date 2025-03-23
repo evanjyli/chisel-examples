b65d4a5240616c9324e6a4ee07d3007b2ce604c9
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-03-23 12:03:21 -0700

    Update gitignore

---
+++ src/main/scala/gcd/DecoupledMux.scala
@@ -0,0 +1,45 @@
+package decoupledmux
+
+import chisel3._
+import chisel3.util._
+import chisel3.util.Decoupled
+import _root_.circt.stage.ChiselStage
+import java.io.PrintWriter
+
+
+class Aggregate extends Bundle {
+  val x = UInt(2.W)
+  val y = SInt(2.W)
+}
+
+class DecoupledMux extends Module {
+  val io = IO(new Bundle {
+    val a = Flipped(Decoupled(new Aggregate))
+    val b = Flipped(Decoupled(new Aggregate))
+    val c = Decoupled(new Aggregate)
+  })
+
+  io.c.bits := Mux(io.a.valid && io.c.ready, io.a.bits,
+                 Mux(io.b.valid && io.c.ready, io.b.bits,
+                   DontCare))
+  io.c.valid := io.a.valid || io.b.valid
+  io.a.ready := io.c.ready
+  io.b.ready := io.c.ready && !io.a.valid
+}
+
+
+object DecoupledMux extends App {
+  ChiselStage.emitSystemVerilogFile(
+    new DecoupledMux,
+    firtoolOpts = Array("-disable-all-randomization",
+      "-strip-debug-info",
+      "--lowering-options=disallowLocalVariables,noAlwaysComb,verifLabels,disallowPortDeclSharing")
+  )
+
+  val chirrtl = ChiselStage.emitCHIRRTL(
+    new DecoupledMux
+  )
+  val fileWriter = new PrintWriter("DecoupledMux.fir")
+  fileWriter.write(chirrtl)
+  fileWriter.close()
+}
